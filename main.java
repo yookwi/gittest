9dfdbfa7508f4469dd89a8b8380c46ead15d3a67
yookwi <yookwi8@gmail.com>
2024-11-03 13:05:55 +0900

    make add Function

--- main.java
+++ main.java
@@ -2,5 +2,8 @@ public class main {
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("hello2");
+
+        int a = 1, b = 1;
+        System.out.println(a + b);
     }
 }
